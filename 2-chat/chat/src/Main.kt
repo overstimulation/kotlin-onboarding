a1011ff4794693b7edda30d8a7108266031e1e1f
overstimulation <146885536+overstimulation@users.noreply.github.com>
2025-06-21 22:44:17 +0200

    Respond to user and ask second question

--- 2-chat/chat/src/Main.kt
+++ 2-chat/chat/src/Main.kt
@@ -1,4 +1,7 @@
 fun main() {
     println("Hello! I'm glad to meet you, let me get to know you better! What is your name?")
     val firstUserAnswer: String? = readlnOrNull()
+
+    println("Nice to meet you, $firstUserAnswer! My name is Kotlin Bot! I am a young programming language created in 2010. How old are you?")
+    val secondUserAnswer: String? = readlnOrNull()
 }
