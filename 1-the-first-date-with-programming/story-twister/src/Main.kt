743a233912cf4b3809eff1b842a6bf212de6fc7e
overstimulation <146885536+overstimulation@users.noreply.github.com>
2025-06-21 22:16:00 +0200

    Add variables for user answers

--- 1-the-first-date-with-programming/story-twister/src/Main.kt
+++ 1-the-first-date-with-programming/story-twister/src/Main.kt
@@ -1,4 +1,8 @@
 fun main() {
     println("Hello! I will ask you several questions.")
     println("Please answer all of them and be honest with me!")
+
+    val firstUserAnswer: String = ""
+    val secondUserAnswer: String = ""
+    val thirdUserAnswer: String = ""
 }
