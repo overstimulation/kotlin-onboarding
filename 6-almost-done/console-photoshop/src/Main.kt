dfee2bb181470a3360a372568970f68d0c757bdc
overstimulation <146885536+overstimulation@users.noreply.github.com>
2025-06-22 11:47:06 +0200

    Add stubs for borders and squared filters

--- 6-almost-done/console-photoshop/src/Main.kt
+++ 6-almost-done/console-photoshop/src/Main.kt
@@ -1,5 +1,9 @@
 fun trimPicture(picture: String): String = picture.trimIndent()
 
+fun applyBordersFilter(picture: String): String = TODO()
+
+fun applySquaredFilter(picture: String): String = TODO()
+
 fun main() {
     // Uncomment this code on the last step of the game
 
