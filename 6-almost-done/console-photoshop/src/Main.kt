5cb6db63de180f981a7cbbcabdf8a9b4f4df05ba
overstimulation <146885536+overstimulation@users.noreply.github.com>
2025-06-22 11:52:55 +0200

    Add applyFilter function to select picture filters

--- 6-almost-done/console-photoshop/src/Main.kt
+++ 6-almost-done/console-photoshop/src/Main.kt
@@ -1,5 +1,15 @@
 fun trimPicture(picture: String): String = picture.trimIndent()
 
+fun applyFilter(picture: String, filter: String): String {
+    val trimmedPicture = trimPicture(picture)
+
+    return when (filter) {
+        "borders" -> applyBordersFilter(trimmedPicture)
+        "squared" -> applySquaredFilter(trimmedPicture)
+        else -> error("Unrecognised filter name")
+    }
+}
+
 fun applyBordersFilter(picture: String): String = TODO()
 
 fun applySquaredFilter(picture: String): String = TODO()
